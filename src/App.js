21db839f62bbaee4e1038e77f6ec27c4ee852b73
GuidoRial <rialguido@gmail.com>
2022-03-11 18:05:49 -0300

    implemented routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import './App.css';
+import { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import SignUp from "./components/SignUp/SignUp";
+import LogIn from "./components/LogIn/LogIn";
+import Board from "./components/Board/Board";
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+    const [user, setUser] = useState(false);
+    const [activeUser, setActiveUser] = useState(false);
+
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Routes>
+                    {user ? (
+                        <Route path="/" element={<Board />} />
+                    ) : (
+                        <Route path="login" element={<LogIn />} />
+                    )}
+                    <Route path="signup" element={<SignUp />} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
