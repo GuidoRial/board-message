b1c329014ec9924c0bf6654ca250c1b83f4221c6
FedeSca001 <familadore@gmail.com>
2022-03-17 13:42:42 -0300

    login function

--- src/components/LogIn/LogIn.js
+++ src/components/LogIn/LogIn.js
@@ -1,82 +1,52 @@
-import React, { useState, useEffect } from 'react'
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import React, { useState } from 'react'
 import './LogIn.css'
 
 const LogIn = () => {
-  const [username, setUsername] = useState('')
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState('')
-  const [checkBox, setCheckBox] = useState(false)
-  const [userValid, setUserValid] = useState(false)
-  const [passwordValid, setpasswordValid] = useState(false)
+  
+  const handleLogIn = async (e) => {
+    e.preventDefault()
+    try {
+      const auth = await getAuth()
+    signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+          const user = userCredential.emailAddress
+          console.log(user)
+        })
+    } catch (error) {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorMessage + errorCode);
+    };
 
-  //Validar username
-  useEffect(()=>{
-    //acá verifica que exista el usuario
-    if (username.length < 12 &&
-        username.length > 5){
-          setUserValid(true)
-    }
-  },[username])
-
-  //Validar password
-  useEffect(()=>{
-    //acá verifica que exista el password
-    if (password.length < 12 &&
-        password.length > 5){
-      setpasswordValid(true)
-    }
-  },[password])
-  //CheckBox hay que ver porque no vuelve al false
-  useEffect(()=>{
-    console.log(checkBox);
-  },[checkBox])
-
-  //Una ves se valido el usuario y la password, escucha el VALID y da autorización
-  useEffect(()=>{
-    if(userValid){
-    }
-  },[userValid])
-
-  useEffect(()=>{
-    if(passwordValid){
-    }
-  },[passwordValid])
-
-
-  function handleLogIn (){
-    if(passwordValid && userValid){
-      let userLogIn = username + password
-      console.log(userLogIn);
-    }
   }
 
   return (
   <section className="sign-up">
-      <h1>BoardMessage</h1>
+      <h1>BoardMessage / Log In</h1>
       <div className="form-container">
-        <form>
+        <form onSubmit={handleLogIn}>
           <input
             className="input-field"
-            id="username"
+            id="email"
             type="text"
-            placeholder="User Name"
-            onChange={e => setUsername(e.target.value)}/>
+            placeholder="E-mail"
+            onChange={e => setEmail(e.target.value)}/>
           <input
             className='input-field'
             id="password"
             type='password'
             placeholder='Password'
             onChange={e => setPassword(e.target.value)}/>
-          <label>You are Human</label>
-          <input
-            className='check-box'
-            type='checkbox'
-            onChange={e => setCheckBox(true)}/>
           <button 
             type="submit"
-            id="create-account"
-            onClick={handleLogIn}>
+            id="create-account">
               Log In
           </button>
+          <button className="demo-butt">User Demo One</button>
+          <button className="demo-butt">User Demo Two</button>
         </form>
       </div>
   </section>
