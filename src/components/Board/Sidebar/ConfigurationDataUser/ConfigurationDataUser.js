d568287163fa7a1f0e8d07ffce3867828d11804d
FedeSca001 <familadore@gmail.com>
2022-03-21 18:21:11 -0300

    add config user

--- src/components/Board/Sidebar/ConfigurationDataUser/ConfigurationDataUser.js
+++ src/components/Board/Sidebar/ConfigurationDataUser/ConfigurationDataUser.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {  useState } from "react";
 import { signOut } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
 import { authService } from "../../../../firebase";
@@ -6,6 +6,17 @@ import icon from '../../../../assets/editDataUser.png'
 import './ConfigurationDataUser.css'
 
 const ConfigurationDataUser = () => {
+    const [changesUser, setChangesUser] = useState({})
+    const [nameConfig, setNameConfig] = useState('')
+    const [descpConfig, setDescpConfig] = useState('')
+    
+    function addChangesUser(){
+        setChangesUser({
+            name: nameConfig,
+            description: descpConfig
+        })
+        console.log(changesUser)
+    }
     const navigate = useNavigate();
     const handleLogOut = async () => {
         try {
@@ -21,21 +32,27 @@ const ConfigurationDataUser = () => {
             <div className='img-user'>
                 Img User
             </div>
-            <h3 className='name-descp'>Name user</h3>
-            <p className='name-descp'>Description user</p>
+            <h3 className='name-config-data'>Name user</h3>
+            <p className='descp-config-data'>Description user</p>
         </div>
-        <form>
+        <form onSubmit={(e)=>{e.preventDefault()}}>
             <label className='label-config-data'>Add img <img src={icon} className="img-icon-config"/></label>
             <input 
                 type='file'
                 accept=".jpg, .jpeg, .png"
                 id='file'
-                className='custom-file-input'/><br/>
+                className='custom-file-input'
+                onChange={(e)=> {console.log(e.target)}}/><br/>
             <label className='label-config-data'>User Name</label>
-            <input type='text' className='input-name-config'></input><br/>
-            <label className='label-config-data'>Description</label><br/>
+            <input type='text' 
+                    className='input-name-config' 
+                    onChange={(e) => setNameConfig(e.target.value)} /><br/>
+            <label className='label-config-data'
+                    onChange={(e) => setDescpConfig(e.target.value)}>Description</label><br/>
             <textarea type='text' className='description-user-data'></textarea><br/>
-            <button type='submit' className='button-config-data'>Send configuration</button>
+            <button type='submit' 
+                    className='button-config-data'
+                    onClick={addChangesUser}>Send configuration</button>
             <button onClick={handleLogOut} className='quit-session'>Quit session</button>
         </form>
     </section>
