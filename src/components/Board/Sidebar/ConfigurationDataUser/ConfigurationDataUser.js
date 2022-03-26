1aa85022a9f8d7e60bc35763887db09bb6976991
FedeSca001 <familadore@gmail.com>
2022-03-25 23:48:59 -0300

    add updateProfile

--- src/components/Board/Sidebar/ConfigurationDataUser/ConfigurationDataUser.js
+++ src/components/Board/Sidebar/ConfigurationDataUser/ConfigurationDataUser.js
@@ -1,25 +1,29 @@
 import React, {  useState } from "react";
-import { signOut } from "firebase/auth";
+import { signOut, getAuth, updateProfile } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
-import { authService, firestore } from "../../../../firebase";
+import { authService } from "../../../../firebase";
 import icon from '../../../../assets/editDataUser.png'
 import './ConfigurationDataUser.css'
 
 const ConfigurationDataUser = ({ activeUser }) => {
     const [newPhoto, setNewPhoto] = useState(activeUser.profilePicture)
-    const [nameConfig, setNameConfig] = useState(activeUser.nae)
-    const [passConfig, setPassConfig] = useState('')
+    const [nameConfig, setNameConfig] = useState(activeUser.name)
+//    const [passConfig, setPassConfig] = useState('')
     const [descpConfig, setDescpConfig] = useState(activeUser.about)
 
     const addChangesUser = async (e)=>{
         e.preventDefault();
-        /*const addChanges = await firestore.colection('users').doc(activeUser.docId).update({
+        const auth = getAuth();
+        // Metodo updateProfile - - - Ver cambios del usuario
+        await updateProfile(auth.currentUser,{
             username: nameConfig,
+            profilePicture: newPhoto,
             about: descpConfig
-        })*/
-        console.log(descpConfig + nameConfig + passConfig + newPhoto);
+        })
+        console.log(auth.currentUser);
     }
     const navigate = useNavigate();
+
     const handleLogOut = async () => {
         try {
             signOut(authService);
@@ -35,7 +39,7 @@ const ConfigurationDataUser = ({ activeUser }) => {
             </div>
             <h3 className='name-config-data'>{activeUser.username}</h3>
             <p className='descp-config-data'>{activeUser.about}</p>
-        <form onSubmit={(e)=>{e.preventDefault()}}>
+        <form onSubmit={(e)=>{addChangesUser(e)}}>
             <label className='label-config-data'>
                 Add img <img src={icon} alt='icon' className="img-icon-config"/>
             </label>
@@ -53,8 +57,7 @@ const ConfigurationDataUser = ({ activeUser }) => {
                     onChange={(e) => setNameConfig(e.target.value)} /><br/>
             <label className='label-config-data'>Password</label>
             <input type='password'
-                    className='input-name-config' 
-                    onChange={(e) => setPassConfig(e.target.value)} /><br/>
+                    className='input-name-config' /><br/>
             <label className='label-config-data'>Description</label><br/>
             <textarea type='text'
                     maxLength={100}
@@ -62,8 +65,7 @@ const ConfigurationDataUser = ({ activeUser }) => {
                     onChange={(e) => setDescpConfig(e.target.value)}></textarea>
             <p className="count-characters">Characters {descpConfig.length} / 100</p><br/>
             <button type='submit' 
-                    className='button-config-data'
-                    onClick={addChangesUser}>Send configuration</button>
+                    className='button-config-data'>Send configuration</button>
             <button onClick={handleLogOut} className='quit-session'>Quit session</button>
         </form>
     </section>
